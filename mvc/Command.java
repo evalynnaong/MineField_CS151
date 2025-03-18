601f694891475d2d5b8eaead574b8fe3148b99cc
evalynnaong <evalynna.ong@gmail.com>
2025-03-17 21:30:43 -0700

    Delete mvc directory

--- mvc/Command.java
+++
@@ -1,8 +0,0 @@
-package mvc;
-
-public abstract class Command extends Model{
-    protected Model model;
-    public Command(Model model) {}
-    public abstract void execute();
-    //public abstract void undo();
-}
